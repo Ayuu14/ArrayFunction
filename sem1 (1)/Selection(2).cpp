c3e6c5d38939ddc35a892334fa6d0e388d87ede4
Ayuu14 <ezreenwayu@gmail.com>
2026-05-02 21:33:51 +0800

    Another exrercise of coding of topic selection

---
+++ sem1 (1)/Selection(2).cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+	int type;
+	double length;
+	cout << "Enter the length (cm):";
+	cin >> length;
+
+	if (length < 0)
+		cout << "Invalid measurement ";
+	else {
+		if (length <= 30)
+			type = 1;
+		else if (length > 30 && length <= 50)
+			type = 2;
+		else if (length > 50 && length <= 70)
+			type = 3;
+		else
+			type = 4;
+
+		switch (type) {
+		case 1:cout << "type 1 " << "Ready to export " << endl;
+			break;
+
+		case 2:cout << "type 2 " << "Ready to material " << endl;
+			break;
+
+		case 3:cout << "type 3 " << "Open selling " << endl;
+			break;
+
+		case 4:cout << "type 4 " << "Recycle to community " << endl;
+			break;
+
+		}
+	}
+	return 0;
+}
