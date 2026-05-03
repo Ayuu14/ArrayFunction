66af609f0d5a02d39a0b2c5ec4eddabe6dc0d61a
Ayuu14 <ezreenwayu@gmail.com>
2026-05-03 10:05:24 +0800

    small exercise of coding about multiplicand

--- sem1 (1)/Exercise3.cpp
+++ sem1 (1)/Exercise3.cpp
@@ -1,24 +1,24 @@
-//#include <iostream> 
-//using namespace std;
-//
-//int main() {
-//	int multiplier, multiplicand, count, product;
-//	cout << "Set the maximum of the multiplier :";
-//	cin >> multiplier;
-//	cout << "Set the multiplicand : ";
-//	cin >> multiplicand;
-//
-//	cout << "\n------------\n";
-//	cout << multiplicand << " Times table\n";
-//	cout << "-------------\n";
-//
-//	count = 1;
-//	while (count <= multiplier) {
-//		product = count * multiplier;
-//		cout << count << " X " << multiplicand << " = " << product << endl;
-//		count = count + 1;
-//
-//	}
-//	return 0;
-//
-//}
+#include <iostream> 
+using namespace std;
+
+int main() {
+	int multiplier, multiplicand, count, product;
+	cout << "Set the maximum of the multiplier :";
+	cin >> multiplier;
+	cout << "Set the multiplicand : ";
+	cin >> multiplicand;
+
+	cout << "\n------------\n";
+	cout << multiplicand << " Times table\n";
+	cout << "-------------\n";
+
+	count = 1;
+	while (count <= multiplier) {
+		product = count * multiplier;
+		cout << count << " X " << multiplicand << " = " << product << endl;
+		count = count + 1;
+
+	}
+	return 0;
+
+}
