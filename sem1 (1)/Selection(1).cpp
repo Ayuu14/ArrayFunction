e0bf389b1a39afc779b68e60cbccbab065af5fe0
Ayuu14 <ezreenwayu@gmail.com>
2026-05-03 10:12:18 +0800

    exercise of coding of topic selection

--- sem1 (1)/Selection(1).cpp
+++ sem1 (1)/Selection(1).cpp
@@ -1,20 +1,20 @@
-//#include<iostream> 
-//using namespace std;
-//int main() {
-//	double length;
-//	cout << "Enter the length (cm):";
-//	cin >> length;
-//	if (length < 0)
-//		cout << "Invalid measurement ";
-//	else
-//		if (length <= 30)
-//			cout << "Approved to export ";
-//		else if (length > 30 && length <= 50)
-//			cout << "Ready to material ";
-//		else if (length > 50 && length <= 70)
-//			cout << "Open to selling ";
-//		else
-//			cout << "Recycle to community ";
-//	return 0;
-//
-//}
+#include<iostream> 
+using namespace std;
+int main() {
+	double length;
+	cout << "Enter the length (cm):";
+	cin >> length;
+	if (length < 0)
+		cout << "Invalid measurement ";
+	else
+		if (length <= 30)
+			cout << "Approved to export ";
+		else if (length > 30 && length <= 50)
+			cout << "Ready to material ";
+		else if (length > 50 && length <= 70)
+			cout << "Open to selling ";
+		else
+			cout << "Recycle to community ";
+	return 0;
+
+}
