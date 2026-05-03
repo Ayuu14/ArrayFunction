0c9443b16c3ea86b8d08504c03825ad941b02ba4
Ayuu14 <ezreenwayu@gmail.com>
2026-05-03 10:06:49 +0800

    Coding of using the rand() to generate random number

--- sem1 (1)/Exercise4.cpp
+++ sem1 (1)/Exercise4.cpp
@@ -1,25 +1,25 @@
-//#include <iostream> 
-//#include <cstdlib> 
-//#include <ctime> 
-//using namespace std;
-//int main() {
-//	int number1, number2, userAnswer, actualAnswer;
-//	char choice;
-//	do {
-//		srand(time(0));
-//		number1 = rand() % 10 + 1;
-//		number2 = rand() % 10 + 1;
-//		cout << "\nWhat is the answer of " << number1 << " X " << number2 << " ? : "; 
-//		cin >> userAnswer;
-//
-//		actualAnswer = number1 * number2;
-//
-//		if (userAnswer == actualAnswer)
-//			cout << "Congratulation\n";
-//		else
-//			cout << "The correct answer is " << actualAnswer << endl;
-//		cout << "Do you want to continue? (Y|N) : ";
-//		cin >> choice;
-//	} while (choice == 'Y');
-//	return 0;
-//}
+#include <iostream> 
+#include <cstdlib> 
+#include <ctime> 
+using namespace std;
+int main() {
+	int number1, number2, userAnswer, actualAnswer;
+	char choice;
+	do {
+		srand(time(0));
+		number1 = rand() % 10 + 1;
+		number2 = rand() % 10 + 1;
+		cout << "\nWhat is the answer of " << number1 << " X " << number2 << " ? : "; 
+		cin >> userAnswer;
+
+		actualAnswer = number1 * number2;
+
+		if (userAnswer == actualAnswer)
+			cout << "Congratulation\n";
+		else
+			cout << "The correct answer is " << actualAnswer << endl;
+		cout << "Do you want to continue? (Y|N) : ";
+		cin >> choice;
+	} while (choice == 'Y');
+	return 0;
+}
